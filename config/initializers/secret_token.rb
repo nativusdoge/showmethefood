fd88d792f472db24b8166e5928e9b233f4348138
Arthur Gunawan <Arthur@Hippo.bigpond>
2012-01-01 12:53:47 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showmethefood::Application.config.secret_token = '4176bb76e2f5ecb469ca00b33cfb5f53741ba4c19cff33859e8d61f308850c3e066428876286c7e46e4a33c154879f90873e7b4018a0a83f5d1cef39147025ab'
